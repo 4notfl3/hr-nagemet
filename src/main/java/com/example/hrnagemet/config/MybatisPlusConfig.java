9219f7d5b5a749d32844ebae834e3423d69a0250
notfls <k6122053@gmail.com>
2025-12-30 11:37:09 +0800

    重构了数据库结构，并修改了对应的代码

---
+++ src/main/java/com/example/hrnagemet/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.example.hrnagemet.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author notfl
+ * @date 2025/12/30 11:20
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
+        return  interceptor;
+    }
+
+}
